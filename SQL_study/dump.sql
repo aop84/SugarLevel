64cc1e362e500cdbe8b489cd955848dfd9d67a54
aop84 <30294477+aop84@users.noreply.github.com>
2017-07-22 19:46:22 +0300

    Adding the SQL folder

---
+++ SQL_study/dump.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE public."SugarLevel"
+(
+	id integer NOT NULL,
+  sugar_level numeric(4,2) NOT NULL,
+  state text NOT NULL,
+  date date NOT NULL,
+  username text NOT NULL,
+  country text NOT NULL
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."SugarLevel"
+  OWNER TO postgres;
+  
+  ALTER TABLE public."SugarLevel"
+  ADD CONSTRAINT "SugarLevel_pkey" PRIMARY KEY(id);
+  
+INSERT INTO public."SugarLevel"(sugar_level, state, date, id, username, country) 
+VALUES (11, 'sf', '2/2/1983', 19, 'Andrey', 'Russia');
+INSERT INTO public."SugarLevel"(sugar_level, state, date, id, username, country) 
+VALUES (11, 'sf', '2/2/1983', 20, 'Andrey', 'Russia');
+INSERT INTO public."SugarLevel"(sugar_level, state, date, id, username, country) 
+VALUES (11, 'sf', '2/2/1983', 21, 'Andrey', 'Russia');
+INSERT INTO public."SugarLevel"(sugar_level, state, date, id, username, country) 
+VALUES (11, 'sf', '2/2/1983', 22, 'Andrey', 'Russia');
+INSERT INTO public."SugarLevel"(sugar_level, state, date, id, username, country) 
+VALUES (11, 'sf', '2/2/1983', 23, 'Andrey', 'Russia');
