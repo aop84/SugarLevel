654313970b67b28f3ac0dd4cd7a8f68679bcc606
aop84 <30294477+aop84@users.noreply.github.com>
2017-07-30 13:20:21 +0300

    Denver delete

--- usr/local/apache/conf/cert/generate.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-..\..\bin\openssl genrsa -out privkey.pem 512
-..\..\bin\openssl req -new -key privkey.pem -out server.csr
-..\..\bin\openssl x509 -req -days 10000 -in server.csr -signkey privkey.pem -out server.crt
-
-
-
